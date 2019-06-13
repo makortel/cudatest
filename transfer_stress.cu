5e4ba3594d885871c72de673f125c9543dc38634
Matti Kortelainen <matti.kortelainen@cern.ch>
2019-06-13 17:11:28 +0200

    Towards multithreaded stress test

--- transfer_stress.cu
+++ transfer_stress.cu
@@ -6,16 +6,42 @@
 #include <chrono>
 #include <random>
 #include <tuple>
+#include <thread>
 
-constexpr size_t TIMES = 1;
+constexpr size_t THREADS = 1;
+constexpr size_t TIMES = 10;
 constexpr size_t MAX = 1<<27; // 128Melements = 512 MB
 constexpr size_t MAXOPS = 1000000;
 
-double transfer(float *a_d, float *a_h, cudaStream_t& stream) {
+struct Data {
+  Data() {
+    cudaStreamCreate(&stream);
+
+    cudaMalloc(&a_d, MAX*sizeof(float));
+    cudaMallocHost(&a_h, MAX*sizeof(float));
+
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::uniform_real_distribution<float> dis(1e-5, 100.);
+    for(size_t i=0; i<MAX; ++i) {
+      a_h[i] = dis(gen);
+    }
+  }
+  ~Data() {
+    cudaFreeHost(a_h);
+    cudaFree(a_d);
+    cudaStreamDestroy(stream);
+  }
+  cudaStream_t stream;
+  float *a_d;
+  float *a_h;
+};
+
+double transfer(Data& data) {
   auto start = std::chrono::high_resolution_clock::now();
 
   for(size_t i=0, j=0; i<MAXOPS; ++i) {
-    cudaMemcpyAsync(a_d+j, a_h+j, sizeof(float), cudaMemcpyDefault, stream);
+    cudaMemcpyAsync(data.a_d+j, data.a_h+j, sizeof(float), cudaMemcpyDefault, data.stream);
     j = (j+1)%MAX;
   }
 
@@ -23,37 +49,18 @@ double transfer(float *a_d, float *a_h, cudaStream_t& stream) {
   return static_cast<double>(std::chrono::duration_cast<std::chrono::microseconds>(stop-start).count())/1e6;
 }
 
-
 int main() {
-  cudaStream_t stream;
-  cudaStreamCreate(&stream);
-
-  float *a_d;
-  float *a_h;
-  cudaMalloc(&a_d, MAX*sizeof(float));
-  cudaMallocHost(&a_h, MAX*sizeof(float));
-
-  std::random_device rd;
-  std::mt19937 gen(rd());
-  std::uniform_real_distribution<float> dis(1e-5, 100.);
-  for(size_t i=0; i<MAX; ++i) {
-    a_h[i] = dis(gen);
-  }
+  Data data;
 
   double total = 0;
   for(size_t i=0; i<TIMES; ++i) {
-    total += transfer(a_d, a_h, stream);
+    total += transfer(data);
   }
   total = total / TIMES;
 
   std::cout << "Ops " << MAXOPS << " time " << total << " ops/s " << (MAXOPS/total) << " us/op " << (total/MAXOPS*1e6) << std::endl;
 
-  cudaStreamSynchronize(stream);
-
-  cudaFreeHost(a_h);
-  cudaFree(a_d);
-
-  cudaStreamDestroy(stream);
+  cudaStreamSynchronize(data.stream);
 
   return 0;
 }
