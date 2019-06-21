647bf2a0d7867cb09978c2be9f20055428ec58e3
Matti Kortelainen <matti.kortelainen@cern.ch>
2019-06-21 20:51:20 +0200

    Monitor also launch time per chunk

--- transfer_kernel_stress.cu
+++ transfer_kernel_stress.cu
@@ -48,14 +48,15 @@ struct Data {
     thread = std::thread{work, this, loops};
   }
 
-  double wait() {
+  std::tuple<double, double> wait() {
     thread.join();
-    return time;
+    return std::make_tuple(time, launch_time);
   }
 
   std::thread thread;
   cudaStream_t stream;
   double time;
+  double launch_time;
   float *a_d;
   float *a_h;
 };
@@ -72,20 +73,26 @@ __global__ void kernel_looping(float *a, unsigned int size, size_t loops) {
 }
 
 void work(Data *data, size_t loops) {
+  double launch_time = 0;
+
   auto start = std::chrono::high_resolution_clock::now();
 
   for(size_t i=0; i<MAXCHUNKS; ++i) {
-    {
+    auto starti = std::chrono::high_resolution_clock::now();
+    //{
       std::lock_guard<std::mutex> lock{cudaMutex};
       cudaMemcpyAsync(data->a_d, data->a_h, SIZE*sizeof(float), cudaMemcpyDefault, data->stream);
       kernel_looping<<<1, CUDATHREADS, 0, data->stream>>>(data->a_d, SIZE, loops);
       cudaMemcpyAsync(data->a_h, data->a_d, SIZE*sizeof(float), cudaMemcpyDefault, data->stream);
-      cudaStreamSynchronize(data->stream);
-    }
+      //}
+    auto stopi = std::chrono::high_resolution_clock::now();
+    launch_time += static_cast<double>(std::chrono::duration_cast<std::chrono::microseconds>(stopi-starti).count())/1e6;
+    cudaStreamSynchronize(data->stream);
   }
 
   auto stop = std::chrono::high_resolution_clock::now();
   data->time = static_cast<double>(std::chrono::duration_cast<std::chrono::microseconds>(stop-start).count())/1e6;
+  data->launch_time = launch_time;
 }
 
 int main() {
@@ -94,17 +101,26 @@ int main() {
   for(size_t nth=1; nth<=MAXTHREADS; ++nth) {
     std::cout << "Number of threads " << nth << std::endl;
     double total = 0;
+    double total_launch = 0;
     for(size_t i=0; i<TIMES; ++i) {
       std::cout << "Trial " << i << std::endl;
       for(size_t j=0; j<nth; ++j) {
         threads[j].workAsync(LOOPS);
       }
       for(size_t j=0; j<nth; ++j) {
-        total += threads[j].wait();
+        auto times = threads[j].wait();
+        total += std::get<0>(times);
+        total_launch += std::get<1>(times);
       }
     }
     total = total / TIMES;
-    std::cout << "Chunks " << (MAXCHUNKS*nth) << " time/trial " << total << " chunks/s " << (MAXCHUNKS*nth/total) << " chunk/op " << (total/(MAXCHUNKS*nth)*1e6) << std::endl;
+    total_launch = total_launch / TIMES;
+    std::cout << "Chunks " << (MAXCHUNKS*nth)
+              << " time/trial " << total
+              << " chunks/s " << (MAXCHUNKS*nth/total)
+              << " us/chunk " << (total/(MAXCHUNKS*nth)*1e6)
+              << " launch us/chunk " << (total_launch/(MAXCHUNKS*nth)*1e6)
+              << std::endl;
   }
 
   return 0;
